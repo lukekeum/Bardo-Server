71d054968ef56ee11d6e4b3544795c355644f69a
lukekeum <lukekeum11@gmail.com>
2020-12-27 17:12:53 +0900

    Add database connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import './env';
+import 'reflect-metadata';
 import app from './app';
+import connectionOptions from './database';
+import { createConnection } from 'typeorm';
 
 const { PORT = String(5000) } = process.env;
+
+createConnection(connectionOptions).then(() =>
+  console.log('Database connected')
+);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
