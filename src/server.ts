e6731f4d359ce6b94d18dcda54b8b338d21bea27
lukekeum <lukekeum11@gmail.com>
2021-01-02 23:17:25 +0900

    Add discord.js client and login

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,15 @@ import 'reflect-metadata';
 import app from './app';
 import connectionOptions from './database';
 import { createConnection } from 'typeorm';
+import client from './client';
 
 const { PORT = String(5000) } = process.env;
+const { TOKEN = String() } = process.env;
 
 createConnection(connectionOptions).then(() =>
   console.log('Database connected')
 );
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+client.login(TOKEN).then(() => console.log(`Bot is running now`));
